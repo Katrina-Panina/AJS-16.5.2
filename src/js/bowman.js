e70694f3dcd492585da8401032940da8e41a891d
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-06-29 09:53:36 +0300

    fix

--- src/js/bowman.js
+++ src/js/bowman.js
@@ -1,7 +1,7 @@
-import Character from "./character";
+import Character from './character';
 
 export default class Bowman extends Character {
   constructor(name) {
-    super(name, "Bowman");
+    super(name, 'Bowman');
   }
 }
