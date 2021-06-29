653fada0e25d328aff5e088054552aec85dd26f5
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2021-06-29 09:58:33 +0300

    fix

--- src/js/daemon.js
+++ src/js/daemon.js
@@ -1,7 +1,7 @@
-import Character from "./character";
+import Character from './character';
 
 export default class Daemon extends Character {
   constructor(name) {
-    super(name, "Daemon");
+    super(name, 'Daemon');
   }
 }
